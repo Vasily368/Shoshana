d6ff9400dbd6ac28a5e7de3d276274ab95acd7cd
Vasily368 <75068993+Vasily368@users.noreply.github.com>
2024-08-21 17:44:17 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,272 +3,267 @@
 
 new Swiper ('.slider-container',{
 
-    slidesPerView: 2,
+    slidesPerView: 1,
     spaceBetween: 30,
 
 
     navigation: {
         nextEl: '.slider-next',
         prevEl: '.slider-prev',
-    }
-
-    ,
+    },
+    
+    breakpoints: {
+        990: {
+            
+            slidesPerView: 2
+        },
+        
+    },
     
 });
 
 
+/////////////////////////////
+
+let burger = document.querySelector('.burger')
+let cross = document.querySelector('.cross')
+let linkHide = document.querySelectorAll('.nav__link--hide')
+let menuHide = document.querySelector('.menu__hidden')
+
+burger.onclick = function (){
+    menuHide.classList.remove('hide')
+}
 
 
-///////////////////////////анимация
+cross.onclick = function (){
+    menuHide.classList.add('hide')
+}
 
 
-const logo = document.querySelector('.logo')
-const headNav = document.querySelectorAll('.nav__list-link')
-const headTitle = document.querySelector('.header__title')
-const headText = document.querySelectorAll('.header__text')
-const headLink = document.querySelector('.header__link')
+linkHide.forEach(function(item){
+    item.onclick = function (){
+        menuHide.classList.add('hide')
+    }
+})
 
-const babyTitle = document.querySelector('.baby-title')
-const babyTopText = document.querySelectorAll('.baby__text-top')
-const babyText = document.querySelectorAll('.baby__text')
+//////////////////анимация
 
 
+// const logo = document.querySelector('.logo')
+// const headNav = document.querySelectorAll('.nav__list-link')
+// const headTitle = document.querySelector('.header__title')
+// const headText = document.querySelectorAll('.header__text')
+// const headLink = document.querySelector('.header__link')
 
-let splitText = (el) => {
-    el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
-  return `<div class="word">` +
-            m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
-            `</div>`;
-    });
-    return el;
-};
-
-function random(min, max){
-    return (Math.random() * (max - min)) + min;
-  }
-
-let split 
+// const babyTitle = document.querySelector('.baby-title')
+// const babyTopText = document.querySelectorAll('.baby__text-top')
+// const babyText = document.querySelectorAll('.baby__text')
 
 
-/////////////////
 
-// let tl = gsap.timeline()
+// let splitText = (el) => {
+//     el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
+//   return `<div class="word">` +
+//             m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
+//             `</div>`;
+//     });
+//     return el;
+// };
 
-// tl.fromTo('.logo',{
+// function random(min, max){
+//     return (Math.random() * (max - min)) + min;
+//   }
+
+// let split 
+
+
+
+
+// ////////////logo
+
+
+// split = splitText(logo)
+
+// Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
+//     TweenMax.from(el, 1,{
+        
+//         opacity: 0,
+//         y:random(  200,200),
+//         delay: idx * 0.1,
+//         repeat: 0,
+//         duration: 2,
+        
+//     })
+// });
+
+
+
+
+// ////////////head-nav
+
+// headNav.forEach( function(item){
+    
+//         split = splitText(item)
    
-//     duration: 0,
-//     opacity: 0,
+       
+//             Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
+//                 TweenMax.from(el, 1,{
+                    
+//                     opacity: 0,
+//                     y:random(200,200),
+//                     delay: idx * 0.1,
+//                     repeat: 0,
+                   
+//                 })
+//             }) 
     
-// },{
-//    opacity: 1,
-//    duration: 2,
-
-// }).fromTo('.nav__list-link' ,{
-//     duration: 0,
-//     opacity: 0,
-
-// },{
+   
     
-//    opacity: 1,
-//    duration: 2,
-
-// }).fromTo('.header__title',{
-//     duration: 0,
-//     opacity: 0,
-
-// },{
-    
-//    opacity: 1,
-//    duration: 2,
-
 // })
 
 
-////////////logo
+
+// /////////////header-title
 
 
-split = splitText(logo)
+// split = splitText(headTitle)
 
-Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
-    TweenMax.from(el, 1,{
+// Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
+//     TweenMax.from(el, 1,{
         
-        opacity: 0,
-        y:random(  200,200),
-        delay: idx * 0.1,
-        repeat: 0,
-        duration: 2,
-        
-    })
-});
-
-
-
-
-////////////head-nav
-
-headNav.forEach( function(item){
-    
-        split = splitText(item)
-   
+//         opacity: 0,
+//         y:random(200,300),
+//         delay: idx * 0.1,
+//         repeat: 0,
        
-            Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
-                TweenMax.from(el, 1,{
-                    
-                    opacity: 0,
-                    y:random(200,200),
-                    delay: idx * 0.1,
-                    repeat: 0,
-                   
-                })
-            }) 
-    
-   
-    
-})
+//     })
+// })  
 
 
 
-/////////////header-title
+// /////////header-text
 
+// headText.forEach( function(item){
 
-split = splitText(headTitle)
-
-Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
-    TweenMax.from(el, 1,{
-        
-        opacity: 0,
-        y:random(200,300),
-        delay: idx * 0.1,
-        repeat: 0,
-       
-    })
-})  
-
-
-
-/////////header-text
-
-headText.forEach( function(item){
-
-    split = splitText(item)
-    Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
-        TweenMax.from(el, 0.1,{
+//     split = splitText(item)
+//     Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
+//         TweenMax.from(el, 0.1,{
            
-            opacity: 0,
-            delay: idx * 0.1,
-            repeat: 0,
+//             opacity: 0,
+//             delay: idx * 0.1,
+//             repeat: 0,
             
-        })
-    })   
+//         })
+//     })   
 
-});
+// });
     
  
   
 
 
-///////////////section-link
+// ///////////////section-link
 
 
-    gsap.from('.header__link',{
-        duration: 3,
-        opacity:0,
+//     gsap.from('.header__link',{
+//         duration: 3,
+//         opacity:0,
 
-    });
+//     });
  
-////////////baby-title
+// ////////////baby-title
 
     
-split = splitText(babyTitle)
+// split = splitText(babyTitle)
  
-Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
-    TweenMax.from(el, 1,{
+// Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
+//     TweenMax.from(el, 1,{
         
-        opacity: 0,
-        x:random(  -200,-200),
-        delay: idx * 0.1,
-        repeat: 0,
+//         opacity: 0,
+//         x:random(  -200,-200),
+//         delay: idx * 0.1,
+//         repeat: 0,
         
-    })
-});
+//     })
+// });
 
 
-/////////////babyTopText
+// /////////////babyTopText
 
 
-babyTopText.forEach( function(item){
+// babyTopText.forEach( function(item){
    
-    split = splitText(item)
+//     split = splitText(item)
 
-    Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
-        TweenMax.from(el, 1,{
+//     Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
+//         TweenMax.from(el, 1,{
             
-            opacity: 0,
-            y:random(200,300),
-            delay: idx * 0.1,
-            repeat: 0,
+//             opacity: 0,
+//             y:random(200,300),
+//             delay: idx * 0.1,
+//             repeat: 0,
            
-        })
-    }) ;  
+//         })
+//     }) ;  
     
-})
+// })
 
 
-/////////////babyText
+// /////////////babyText
 
 
-babyText.forEach( function(item){
+// babyText.forEach( function(item){
    
-    split = splitText(item)
+//     split = splitText(item)
 
-    Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
-        TweenMax.from(el, 0.1,{
+//     Array.from(split.querySelectorAll('.letter')).forEach((el,idx) =>{
+//         TweenMax.from(el, 0.1,{
             
-            opacity: 0,
-            delay: idx * 0.1,
-            repeat: 0,
+//             opacity: 0,
+//             delay: idx * 0.1,
+//             repeat: 0,
            
-        })
-    }) ;  
+//         })
+//     }) ;  
     
-})
+// })
 
 
-//////////////runningline
+// //////////////runningline
 
-gsap.from('.running-line__text',{
+// gsap.from('.running-line__text',{
    
-    opacity : 0,
-    duration: 3,
+//     opacity : 0,
+//     duration: 3,
     
-}) 
+// }) 
 
-gsap.to('.running-line__text',{
+// gsap.to('.running-line__text',{
    
     
-    x: -2800,
-    duration: 3,
-}) 
+//     x: -2800,
+//     duration: 3,
+// }) 
 
-//////////////about
+// //////////////about
    
-    gsap.to('.about-bg__img',{
+//     gsap.to('.about-bg__img',{
        
-        y:7500,
-        scale: 25,
-        duration: 3,
-        opacity: 0,
-    }) 
+//         y:7500,
+//         scale: 25,
+//         duration: 3,
+//         opacity: 0,
+//     }) 
    
-////////////////////////////
+// ////////////////////////////
 
 
-gsap.to('.header__inner',{
-    scrollTrigger:{
-        trigger: '.header',
-        markers: true,
-        start: 'top top'
-    },
+// gsap.to('.header__inner',{
+//     scrollTrigger:{
+//         trigger: '.header',
+//         markers: true,
+//         start: 'top top'
+//     },
     
-})
+// })
 
